0d0b1324bc164457262ffe1b51f8f308520ef2e9
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-02-18 19:14:22 +0530

    code updated

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 let song = document.querySelector(".song");
-let progress=document.querySelector(".progress");
-let playIcon=document.querySelector(".playIcon");
+let progress = document.querySelector("#progress");
+let playIcon = document.querySelector("#playIcon");
 
-song.onloadedmetadata=function() {
-    progress.max=song.duration;
-    progress.value=song.currentTime;
+let songName=document.querySelector(".song-name");
+let artist=document.querySelector(".artist");
+let songImg=document.querySelector(".song-img");
+let prev=document.querySelector("#prev");
+let next=document.querySelector("#next");
+
+song.onloadedmetadata = function () {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+}
+
+let playPause = () => {
+    if (playIcon.classList.contains("fa-pause")) {
+        song.pause();
+        playIcon.classList.add("fa-play");
+        playIcon.classList.remove("fa-pause");
+    } else {
+        song.play();
+        playIcon.classList.add("fa-pause");
+        playIcon.classList.remove("fa-play");
+    }
+}
+
+document.querySelector(".controls div:nth-child(2").addEventListener("click", playPause);
+
+// code for progress bar
+if (song.play()) {
+    setInterval(() => {
+        progress.value=song.currentTime;
+    }, 800);
+}
+
+progress.onchange=function(){
+    song.currentTime=progress.value;
 }
